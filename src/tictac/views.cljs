a17541ab5d9f6e5ee2f5ba262b955e17606d947d
Joseph Rollins <rollins.joseph+git@gmail.com>
2021-04-17 15:03:39 -0400

    UI changes to match mocks
    - Add always present title to the top
    - Don't vertical center UI which causes layout shifts
    - Absolutlely position start buttons over blank board

--- src/tictac/views.cljs
+++ src/tictac/views.cljs
@@ -7,53 +7,57 @@
 
 (defn cell [{:keys [value on-click]}]
   [:div
-   {:class "w-32 h-32 border-2 border-white-600 rounded bg-white text-6xl
-            flex justify-center items-center flex-col"
+   {:class (str "w-32 h-32 rounded-lg bg-gray-50 text-6xl
+                  flex justify-center items-center flex-col
+                  text-gray-700 "
+                (if on-click
+                  "hover:bg-gray-200 cursor-pointer"
+                  "cursor-default"))
     :on-click #(when on-click (on-click))}
    (str value)])
 
 (defn board [board]
-  [:div {:class "grid grid-cols-3 grid-rows-3 gap-2 p-2 bg-red-100 rounded"}
+  [:div {:class "grid grid-cols-3 grid-rows-3 gap-3 p-3 bg-green-300 rounded-lg"}
    (doall (for [row (range 3)
                 col (range 3)
                 :let [value (e/cell board [row col])]]
             ^{:key [col row]}
             [cell {:value value
-                   :on-click #(when (nil? value)
-                                (re-frame/dispatch [::events/move [row col]]))}]))])
+                   :on-click (when (and (nil? value)
+                                        (not (e/winner? board)))
+                               #(re-frame/dispatch [::events/move [row col]]))}]))])
 
 (defn button
   [{:keys [on-click]} text]
   [:button {:class "py-2 px-4 bg-red-500 text-white font-semibold rounded-lg
-                    shadow-md focus:outline-none focus:ring-2
-                    focus:ring-purple-600 focus:border-transparent
-                    focus:ring-offset-2 focus:ring-offset-purple-200
-                    active:bg-red-800"
+                    focus:outline-none hover:bg-red-600 hover:ring-2
+                    active:bg-red-500"
             :on-click on-click}
    text])
 
 (defn start
   []
-  [:div {:class "flex flex-col container mx-auto items-center justify-center h-screen"}
-   [:div {:class "flex flex-col bg-red-200 p-10 w-1/3 h-1/3 rounded-lg justify-between"}
-    [:h1 {:class "text-6xl text-purple-600 text-center font-bold"} "Tic Tac"]
-    [:div {:class "flex flex-col space-y-4"}
-     [button
-      {}
-      "vs. Player"]
-     [button
-      {:on-click #(re-frame/dispatch [::events/play :computer])}
-      "vs. AI"]]]])
+  [:div {:class "relative"}
+   [board e/empty-board]
+   [:div {:class "absolute inset-0 flex flex-col w-full h-full p-8 space-y-4 justify-center"}
+    [button {} "vs Player"]
+    [button {:on-click #(re-frame/dispatch [::events/play :computer])}
+     "vs AI"]]])
 
 (defn game []
   (let [board-state @(re-frame/subscribe [::subs/board])
         winner (e/winner? board-state)]
-    [:div {:class "flex flex-col container mx-auto items-center justify-center h-screen"}
-     (when winner [:h1 winner])
-     [board board-state]]))
+    (when winner [:h1 winner])
+    [board board-state]))
+
+(defn scaffold [children]
+  [:div {:class "flex flex-col container mx-auto items-center"}
+   [:h1 {:class "font-bold text-6xl py-8 text-gray-700"}
+    "Tic Tac"]
+   children])
 
 (defn router []
   (let [page @(re-frame/subscribe [::subs/page])]
     (case page
-      :start [start]
-      :game [game])))
+      :start [scaffold [start]]
+      :game [scaffold [game]])))
